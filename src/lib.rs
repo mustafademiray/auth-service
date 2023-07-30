d66680169c4c3f40e8626e33b5aa0562df343ca0
mustafademiray <mustafademiray@protonmail.com>
2023-07-30 03:15:26 +0300

    playing around with modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![allow(dead_code, unused_variables)]
+
+
+mod database {
+    pub enum Status {
+        Connected,
+        Interrupted,
+    }
+
+    pub fn connect_to_database() -> Status {
+        //mock setup
+        return Status::Connected;
+    }
+
+    pub fn get_user() {
+        //get user from db
+    }
+
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod auth_utils {
+    
+    pub fn login (creds: models::Credentials) {
+        //authenticate
+        crate::database::get_user();
+    }
+    
+    fn logout () {
+        //log user out
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub mod models {
+        pub struct Credentials {
+            username: String,
+            password: String,
+        }
+    }
+
+}
+
+
+pub fn authenticate(creds: auth_utils::models::Credentials) {
+    if let database::Status::Connected = database::connect_to_database() {
+        auth_utils::login(creds);
     }
 }
